04fb6d9deaf5760be180547895ac6ecbccd365fd
Dat Ma XM <datma113112111@gmail.com>
2020-09-14 15:52:18 +0700

    update: sale off products...

---
+++ src/docs/shop/saleOff.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span><p  class="title">Sale Off</p></span>
+    <div class="row mt-5">
+        <saleOffProduct class="col-lg-3" v-for="product in saleOffProducts" v-bind:key="product.id" v-bind:product="product"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import saleOffProduct from './saleOffProduct'
+export default {
+    name: 'saleOff',
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        saleOffProduct
+    },
+    props: {
+        saleOffProducts: {
+            type: Array,
+            default: []
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.title {
+    display: inline-block;
+    font-weight: bold;
+    font-size: 3rem;
+    border-bottom: 3px solid #4caf50;
+    cursor: pointer;
+}
+</style>
--- src/docs/shop/shop.vue
+++ src/docs/shop/shop.vue
@@ -8,7 +8,9 @@
           <price class="mt-5" />
           <colors class="mt-5" /> 
           <popularProducts class="mt-5"/>
-          <latestProducts class="mt-5"/>
+        </div>
+        <div class="col-xl-9">
+          <saleOff v-bind:saleOffProducts="saleOffProducts"/>
         </div>
       </div>
     </div>
@@ -21,11 +23,25 @@ import department from './department'
 import price from './price'
 import colors from './colors'
 import popularProducts from './popularProducts'
-import latestProducts from './latestProducts'
+import saleOff from './saleOff'
 export default {
   name: "shop",
   data() {
-    return {};
+    return {
+      saleOffProducts: [
+        { id: 0, url: 'apple.png', name: 'Fresh apple', type: 'Fruits', originPrice: 100, discount: 0.2 },
+        { id: 1, url: 'banana1.png', name: 'Fresh Banana', type: 'Fruits', originPrice: 250, discount: 0.4 },
+        { id: 2, url: 'berry1.png', name: 'Fresh Berry', type: 'Fruits', originPrice: 600, discount: 0.25 },
+        { id: 3, url: 'buffmeat.png', name: 'Bufferlow meat', type: 'Meat', originPrice: 400, discount: 0.33 },
+        { id: 4, url: 'burger.png', name: 'Humburger', type: 'Fastfood', originPrice: 2200, discount: 0.6 },
+        { id: 5, url: 'carrot.png', name: 'Fresh Carrot', type: 'Vegetables', originPrice: 1400, discount: 0.2 },
+        { id: 6, url: 'drinkfood.png', name: 'Apple juice ', type: 'DrinkFood', originPrice: 300, discount: 0.1 },
+        { id: 7, url: 'cowmeat.png', name: 'Cow Meat', type: 'Meat', originPrice: 600, discount: 0.2 },
+        { id: 8, url: 'ginger.png', name: 'Fresh Ginger', type: 'Vegetables', originPrice: 500, discount: 0.7 },
+        { id: 9, url: 'milk.png', name: 'Fresh milk', type: 'DrinkFood', originPrice: 800, discount: 0.5 },
+        
+      ]
+    };
   },
   components: {
     background,
@@ -33,7 +49,8 @@ export default {
     price,
     colors,
     popularProducts,
-    latestProducts
+saleOff,
+
   }
 };
 </script>
